5a21b3e230538cc1669bdfabd3e04a3604c69f41
chengxinyong <chengxinyong@139.com>
2019-03-16 21:01:07 +0800

    http

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -28,6 +28,10 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
 
   ngOnInit() {
     this.getHero();
